cdfb1f3d9012179ab463b10f56cd23d2229b497d
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-04-10 15:36:02 +0900

    add button start, stop, reset

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+      @IBOutlet weak var timeLabel: UILabel!
+      var count = 0
+      var myTimer = Timer()
+      
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
+       
+      }
+      func updateTime() {
+            count = count + 1
+            let min = count / 60 / 100
+            let sec = (count - (min * 60 * 100)) / 100
+            let msec = count - (min * 60 * 100) - (sec * 100)
+            timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
       }
 
-      override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+      @IBAction func btstart(_ sender: Any) {
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
+                  self.updateTime()
+            })
+          
       }
-
-
+      
+      @IBAction func btstop(_ sender: Any) {
+            myTimer.invalidate()
+      }
+      
+      @IBAction func btreset(_ sender: Any) {
+            timeLabel.text = "00:00:00"
+      }
 }
 
